59936afd1502e957c97d4b73bc316ae1c8e88772
SaurabhAgarwal207 <mcasaurabhagarwal@gmail.com>
2025-03-30 20:43:16 +0530

    EvenOdd

---
+++ src/com/saurabh/basics/EvenOdd.java
@@ -0,0 +1,16 @@
+package com.saurabh.basics;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num =scan.nextInt();
+        if(num%2==0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd Number");
+    }
+
+}
